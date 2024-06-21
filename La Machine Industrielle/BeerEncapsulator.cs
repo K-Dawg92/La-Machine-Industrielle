49d0f4e229aaa2f997a6fec37e2f26de5b12f5c2
K-Dawg92 <kevin.osei.yaw@gmail.com>
2024-06-21 10:25:00 +0200

    Ajoutez des fichiers projet.

---
+++ La Machine Industrielle/BeerEncapsulator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_Machine_Industrielle
+{
+    public class BeerEncapsulator
+    {
+        // those variables below are the properties/attributes of the class, they are a class level scope (since class level variables are declared outside of
+        //any methods, you can use them anywhere inside the class.)
+        private double _availableBeerVolume;
+        private int _availableBottles;
+        private int _availableCapsules;
+
+
+
+        // this below is the constructor 
+        public BeerEncapsulator(double availableBeerVolume, int availableBottles, int availableCapsules)
+        {
+            _availableBeerVolume = availableBeerVolume;
+            _availableBottles = availableBottles;
+            _availableCapsules = availableCapsules;
+        }
+
+
+        public double AvailableBeerVolume
+        {
+            get { return _availableBeerVolume; }
+        }
+        public int AvailableBottles
+        {
+            get { return _availableBottles; }
+            set { _availableBottles = value; }
+        }
+        public int AvailableCapsules
+        {
+            get { return _availableCapsules; }
+            set { _availableCapsules = value; }
+        }
+
+        public void AddBeer(double amount)
+        { 
+            _availableBeerVolume += amount;
+        }
+
+        public int ProduceEncapsulatedBeerBottles(int bottleToProduce)
+        {
+            int bottleProduced = 0;
+            while (bottleProduced != bottleToProduce)
+            {
+                if (_availableBottles > 0 && _availableCapsules > 0 && _availableBeerVolume >= 0.33)
+                {
+                    _availableBottles--;
+                    _availableCapsules--;
+                    _availableBeerVolume -= 0.33;
+                    bottleProduced++;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Sorry inssuficient items to produce a bottle");
+                    if (_availableBeerVolume < 0.33)
+                    {
+                        Console.WriteLine($"Beer problem  {(bottleToProduce - bottleProduced)*0.33}");
+                    }
+                    if (_availableBottles ==0)
+                    {
+                        Console.WriteLine($"Bottle problem {bottleToProduce - bottleProduced}");
+                    }
+                    if (_availableCapsules== 0)
+                    {
+                        Console.WriteLine($"Caps problem {bottleToProduce - bottleProduced}");
+                    }
+                    return 0;
+                    
+                }
+            }
+
+            return bottleProduced;
+        }
+    }
+}
---
+++ La Machine Industrielle/Program.cs
@@ -0,0 +1,27 @@
+﻿using La_Machine_Industrielle;
+public class program
+{
+    public static void Main(String[] args)
+
+    {
+        Console.WriteLine("Please enter the required amount of litres of beer you want?");
+        double availableBeerVolume = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Now enter the amount of empty bottles are required");
+        int availableBottles = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter the amount of caps required, should be the same as empty bottles");
+        int availablecapsules = int.Parse(Console.ReadLine());
+
+        BeerEncapsulator bottle1 = new BeerEncapsulator(availableBeerVolume, availableBottles,availablecapsules);
+        Console.WriteLine($"Available Beer Volume {bottle1.AvailableBeerVolume}");
+        Console.WriteLine($"Available Bottles {bottle1.AvailableBottles}");
+        Console.WriteLine($"Available Caps {bottle1.AvailableCapsules}");
+
+        bottle1.ProduceEncapsulatedBeerBottles(10);
+        bottle1.ProduceEncapsulatedBeerBottles(50);
+        bottle1.ProduceEncapsulatedBeerBottles(500);
+
+    }
+}
+
